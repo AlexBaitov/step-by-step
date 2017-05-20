83ae17c5efe24adcca59451b13ae8689a6b7238c
al-baitov <al.baitov@2gis.ru>
2017-05-20 15:38:15 +0700

    routers

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/takama/router"
 	"net/http"
 )
 
@@ -10,9 +11,34 @@ import (
 // - curl http://localhost:8000
 // - curl http://localhost:8000/test-some-path
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe("localhost:8000", nil)
+
+	r := router.New()
+	r.GET("/home", home)
+	r.GET("/mw", mw(5))
+	r.GET("/mw2", mw2(5, abc))
+
+	r.Listen(":8000")
 }
+
+func home(c *router.Control) {
+	fmt.Fprintf(c.Writer, "URL.Path = %q\n", c.Request.URL.Path)
+
+}
+
+func mw(a int) router.Handle {
+	return func(c *router.Control) {
+		fmt.Fprintf(c.Writer, "a = %v\n", a)
+	}
+}
+
+func mw2(a int, h router.Handle) router.Handle {
+
+	return h
+}
+func abc(c *router.Control) {
+	fmt.Fprintf(c.Writer, "a = %v\n", 6)
+}
+
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
--- main_test.go
+++ main_test.go
@@ -31,4 +31,29 @@ func TestHandler(t *testing.T) {
 	}
 }
 
+func TestHandlerFewCases(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(handler))
+	defer ts.Close()
+
+	var testCases = []string{}
+
+	res, err := http.Get(ts.URL + "/")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	greeting, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expectedGreeting := "URL.Path = \"/\""
+	testingGreeting := strings.Trim(string(greeting), " \n")
+	if testingGreeting != expectedGreeting {
+		t.Fatalf("Wrong greeting '%s', expected '%s'", testingGreeting, expectedGreeting)
+	}
+}
+
 // TODO: add one more test together
